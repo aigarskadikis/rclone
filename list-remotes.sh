ce3f092de258a085132eea155b1c625719d4a76a
catonrug <aigars.kadikis@gmail.com>
2021-01-30 14:38:43 +0200

    next

--- list-remotes.sh
+++ list-remotes.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+FREE_SPACE="start"
+
 rclone listremotes |\
+(
 while IFS= read -r REMOTE
-do {
+do 
+
 echo $REMOTE
-rclone about --full $REMOTE
-} done
+
+ABOUT="$(rclone about --full $REMOTE)"
+echo "$ABOUT" | grep "Free"
+FREE_SPACE="${FREE_SPACE} $(echo "$ABOUT" | grep "Free")"
+
+#rclone lsf --recursive $REMOTE | sed "s|^|$REMOTE\/|"
+
+echo "${FREE_SPACE}"
+
+done 
+
+echo "${FREE_SPACE}"
+)
